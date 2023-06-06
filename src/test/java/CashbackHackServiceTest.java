9aa3ef2801835e465e1145c4921f0c6cd93f04e4
kirillmironov9 <kirill.mironov93@yandex.ru>
2023-06-07 00:12:27 +0300

    add junit4

---
+++ src/test/java/CashbackHackServiceTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+public class CashbackHackServiceTest {
+    @org.junit.Test
+    public void testRemain () {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+    @org.junit.Test
+    public void testRemainCash () {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(expected, actual);
+    }
+  
+}
